75ffb52495c3e8ccc97cfca63fc5715c925b72c5
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-07-09 09:38:03 +0100

    Fix calculate interest as float

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,4 @@ principal = 1000
 rate = 0.05
 time = 3
 interest = (principal * rate * time)
-result = interest
-print(f"The simple interest is: {result}")
+print(f"The simple interest is: {interest}")

39887d8a6715909a3043a5b373d08ab532130b3b
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-07-09 17:37:24 +0100

    Changes on the word for proper understanding

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-user1 = int(input("Enter your monthly income: "))
-user2 = int(input("Enter your total monthly expenses: "))
-savings = user1 - user2
-print(f"Your monthly savings are {savings}.")
-projected_savings = int(savings * 12 + (savings * 12 * 0.05))
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+print(f"Your monthly savings are {monthly_savings}.")
+projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 print(
     f"Projected savings after one year, with interest, is: {projected_savings}.")

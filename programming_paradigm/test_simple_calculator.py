323a59900de12ea6aaac4b742ba57271bf4135b6
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-08-04 06:05:43 +0100

    Done deal

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,27 +3,37 @@ from simple_calculator import SimpleCalculator
 
 
 class TestSimpleCalculator(unittest.TestCase):
+
     def setUp(self):
+        """Set up a SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
+        """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
-        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
         self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(-1, 1), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
+        """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(5, 3), 2)
-        self.assertEqual(self.calc.subtract(0, 3), -3)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
 
-    def test_multiply(self):
-        self.assertEqual(self.calc.multiply(3, 4), 12)
-        self.assertEqual(self.calc.multiply(0, 5), 0)
+    def test_multiplication(self):
+        """Test the multiplication method."""
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
 
-    def test_divide(self):
-        self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(9, 3), 3)
-        self.assertIsNone(self.calc.divide(10, 0))
+    def test_division(self):
+        """Test the division method."""
+        self.assertEqual(self.calc.divide(6, 2), 3)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
+        # division by zero should return None
+        self.assertIsNone(self.calc.divide(10, 0))
+        self.assertEqual(self.calc.divide(-6, -2), 3)
 
 
 if __name__ == '__main__':
